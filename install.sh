b3b196c1db7ff944030e27b9a423b3d082bc9cfd
Oskar Schöldström <public@oxy.fi>
2012-02-15 20:42:20 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -4,15 +4,29 @@
 #
 # From: https://github.com/krisleech/vimfiles/blob/master/bootstrap.sh
 
-read -p "Deleting old vim files. Are you sure? (y/n) "
-echo
-if [[ $REPLY = [Yy] ]]; then
+function install {
+  git clone git://github.com/oxyc/vimrc.git ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc && active_submodules
+}
+
+function activate_submodules {
+  for dir in $(find '~/.vim/bundle' -type d); do
+    cd ~/.vim/bundle/${dir} && git submodule init
+  done
+  cd
+}
+
+function clean {
   cp -r ~/.vim ~/.vim.old 2>/dev/null
   rm -rf ~/.vim 2>/dev/null
   rm -f ~/.vimrc 2>/dev/null
+}
+
+read -p "Deleting old vim files. Are you sure? (y/n) "
+echo
+if [[ $REPLY = [Yy] ]]; then
+  clean && install;
+else
+  install;
 fi
 
-echo 'Cloning git://github.com/oxyc/vimrc.git';
-git clone git://github.com/oxyc/vimrc.git ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc
-
-echo 'Done!';
+echo 'Done!'
