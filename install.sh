f9feeff612f1185bfcb839877e969a0c2aa76c48
Oskar Schöldström <public@oxy.fi>
2012-02-15 21:01:55 +0200

    Cleaned install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Install script
 # curl https://raw.github.com/oxyc/vimrc/master/install.sh -o - | sh
 #
-# From: https://github.com/krisleech/vimfiles/blob/master/bootstrap.sh
+# Inspired by: https://github.com/krisleech/vimfiles/blob/master/bootstrap.sh
 
 function install {
   git clone git://github.com/oxyc/vimrc.git ~/.vim && cd ~/.vim && git submodule init && git submodule update && ln -s ~/.vim/vimrc ~/.vimrc
@@ -14,5 +14,4 @@ function clean {
   rm -f ~/.vimrc 2>/dev/null
 }
 
-clean && install;
-echo 'Done!'
+clean && install
