95b5176755b5ca6427525253339a5659097b9457
Oskar Schöldström <public@oxy.fi>
2012-02-17 23:30:16 +0200

    Updated install with simplified logic and old vimrc backup

--- install.sh
+++ install.sh
@@ -4,14 +4,18 @@
 #
 # Inspired by: https://github.com/krisleech/vimfiles/blob/master/bootstrap.sh
 
-function install {
-  git clone git://github.com/oxyc/vimrc.git ~/.vim && cd ~/.vim && git submodule init && git submodule update && ln -s ~/.vim/vimrc ~/.vimrc
+install () {
+  git clone --recursive git://github.com/oxyc/vimrc.git ~/.vim \
+    && ln -s ~/.vim/vimrc ~/.vimrc
 }
 
-function clean {
+clean () {
   cp -r ~/.vim ~/.vim.old 2>/dev/null
   rm -rf ~/.vim 2>/dev/null
+  cp ~/.vimrc ~/.vimrc.old 2>/dev/null
   rm -f ~/.vimrc 2>/dev/null
 }
 
 clean && install
+unset install
+unset clean
